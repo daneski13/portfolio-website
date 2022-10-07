1f167b580be58331a93a3e101cce0b470c656bb1
Dane Skalski <daneski13@gmail.com>
2022-10-07 12:48:55 -0700

    Create Anchor component for use with markdown posts

---
+++ src/lib/components/markdown/Anchor.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let href: string;
+	export let target: string;
+	export let rel: string = '';
+</script>
+
+<a {href} {target} {rel}><slot /></a>
+
+<style lang="scss">
+	a {
+		color: rgb(69, 69, 255);
+
+		&:hover {
+			text-decoration: underline;
+		}
+	}
+</style>
